ad181d6e382d161e8fb6ab92081bc4876c1b876e
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-12-10 16:16:45 -0500

    nobody does it like treehouse

--- game.py
+++ game.py
@@ -1,47 +1,46 @@
 from cards import Card
 import random
 
-class Game:
 
+class Game:
     def __init__(self):
         self.size = 4
-        self.card_options = ["add", "boo", "Cat", "Dog", 
-                                "Dev", "Egg", "Gum", "Hut"]
-        self.columns = ["A", "B", "C", "D"]
+        self.card_options = ['Add', 'Boo', 'Cat', 'Dev',  'Egg', 'Far', 'Gum', 'Hut']
+        self.columns = ['A', 'B', 'C', 'D']
         self.cards = []
         self.locations = []
         for column in self.columns:
             for num in range(1, self.size + 1):
-                self.locations.append(f'{column} {num}')
+                location = f'{column}{num}'
+                self.locations.append(location)
 
     def set_cards(self):
         used_locations = []
-        for word in self.card_options:
+        for card in self.card_options:
             for i in range(2):
-                available_locations = set(self.locations) - set(used_locations)
-                random_location = random.choice(list(available_locations))
+                random_location = random.choice(list(set(self.locations) - set(used_locations)))
                 used_locations.append(random_location)
-                card = Card(word, random_location)
-                self.cards.append(card)
+                game_card = Card(card, random_location)
+                self.cards.append(game_card)
 
     def create_row(self, num):
         row = []
         for column in self.columns:
             for card in self.cards:
-                if card.location == f'{column} {num}':
+                if card.location == f'{column}{num}':
                     if card.matched:
                         row.append(str(card))
                     else:
-                        row.append("   ")
-        return row
+                        row.append('   ')
+            return row
 
     def create_grid(self):
         header = ' |  ' + '  |  '.join(self.columns) + '  |'
         print(header)
-        for row in range(1, self.size + 1):
+        for row in range(1, (self.size + 1)):
             print_row = f'{row}| '
             get_row = self.create_row(row)
-            print_row += " | ".join(get_row) + ' |'
+            print_row += ' | '.join(get_row) + ' |'
             print(print_row)
 
     def check_match(self, loc1, loc2):
@@ -49,18 +48,18 @@ class Game:
         for card in self.cards:
             if card.location == loc1 or card.location == loc2:
                 cards.append(card)
-        if cards[0] == cards[1]:
-            cards[0].matched = True
-            cards[1].matched = True
-            return True
-        else: 
-            for card in cards:
-                print(f'{card.location}: {card}')
-            return False
+            if cards[0] == cards[1]:
+                cards[0].matched = True
+                cards[1].matched = True
+                return True
+            else:
+                for card in cards:
+                    print(f'{card.location}: {card}')
+                return False
 
     def check_win(self):
         for card in self.cards:
-            if card.matched == False:
+            if card.matched is False:
                 return False
         return True
 
@@ -70,17 +69,26 @@ class Game:
             if guess.upper() in self.locations:
                 return guess.upper()
             else:
-                print("That's not a valid location, try something like 'A1'")
+                print("That's not a valid location. Location should be column name then row name like A1.")
+
+    def start_game(self):
+        game_running = True
+        print("Memory Game")
+        self.set_cards()
+        while game_running:
+            self.create_grid()
+            guess1 = self.check_location("first")
+            guess2 = self.check_location("second")
+            if self.check_match(guess1, guess2):
+                if self.check_win():
+                    print("Congrats, you win")
+                    self.create_grid()
+                    game_running = False
+            else:
+                input('those cards are not a match.  Press enter to continue')
+
+        print("Game Over")
 
 if __name__ == '__main__':
     game = Game()
-    game.set_cards()
-    game.create_grid()
-    # game.cards[0].matched = True
-    # game.cards[1].matched = True
-    # game.cards[2].matched = True
-    # game.cards[3].matched = True
-    # print(game.create_row(1))
-    # print(game.create_row(2))
-    # print(game.create_row(3))
-    # print(game.create_row(4))
+    game.start_game()
